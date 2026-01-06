cb8c046d5c03b7decf80812efd03e37a7147c364
Diki Hermawan <dikihermawan22082021@gmail.com>
2026-01-06 15:46:23 +0800

    Add migration files for roles, categories, items, orders, and order_items tables

---
+++ database/migrations/2026_01_06_085430_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_code')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('sub_total', 10, 2);
+            $table->decimal('tax', 10, 2);
+            $table->decimal('grand_total', 10, 2);
+            $table->enum('status', ['pending', 'processing', 'completed', 'cancelled']);
+            $table->integer('table_number');
+            $table->enum('payment_method', ['cash', 'qris']);
+            $table->text('notes')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
