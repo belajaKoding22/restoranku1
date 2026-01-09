3a515ef3cd6f87f351ec89df1ee833382663a7b3
Diki Hermawan <dikihermawan22082021@gmail.com>
2026-01-09 11:22:44 +0800

    Refactor Item and User models to include HasFactory trait and remove unnecessary comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'username',
         'password',
